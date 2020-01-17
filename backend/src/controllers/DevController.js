da33ef330c8ef3059a63622bdba0fd409ec50b7e
MarcelloA <marcellooliveira_25@yahoo.com.br>
2020-01-16 22:57:22 -0300

    Base da Aplicação (completo)

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,32 +1,49 @@
+/*  Funcoes do Controller:
+index, show, store, update, destroy
+*/  
+
 const axios = require("axios"); //  chamadas para outras APIs
-const Dev = require("../models/Dev")
+const Dev = require("../models/Dev");
+const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
+    async index(request, response) {
+      const devs = await Dev.find();
+
+      return response.json(devs);
+    },
+
+
+  // async => pode demorar a responder
     async store(request, response) {
         const { github_username, techs, latitude, longitude } = request.body; // buscando 'github_username' dentro de 'request.body'
-        // await => esperar o 'axios.get' finalizar
-        const apiResponse = await axios.get(
-          `https://api.github.com/users/${github_username}`
-        ); // estudar 'promiss'
+        
+        let dev = await Dev.findOne({ github_username });
+
+        if(!dev){
+          // await => esperar o 'axios.get' finalizar
+          const apiResponse = await axios.get(
+            `https://api.github.com/users/${github_username}`
+          ); // estudar 'promiss'
       
-        const { name = login, avatar_url, bio } = apiResponse.data;
+          const { name = login, avatar_url, bio } = apiResponse.data;
       
-        const techsArray = techs.split(",").map(tech => tech.trim());
+          const techsArray = parseStringAsArray(techs);
       
-        const location = {
-          type: 'Point',
-          coordinates: [longitude, latitude],
+          const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          }
+      
+          dev = await Dev.create({
+            github_username,
+            name,
+            avatar_url,
+            bio,
+            techs: techsArray,
+            location,
+          })
         }
-      
-        const dev = await Dev.create({
-          github_username,
-          name,
-          avatar_url,
-          bio,
-          techs: techsArray,
-          location,
-        })
-      
         return response.json(dev);
       }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,12 @@
 const { Router } = require("express"); // importando apenas o modulo de roteamento
 const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
-// async => pode demorar a responder
+
+routes.get('/devs', DevController.index);
 routes.post("/devs", DevController.store);
+routes.get('/search', SearchController.index);
+
 
 module.exports = routes;
